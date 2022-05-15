635641b1eb78ccb8310944be121308e6a3de433b
ylinafernandao <lfotalora@uniboyaca.edu.co>
2022-05-15 00:02:47 -0500

    actualizar

---
+++ actualizar.php
@@ -0,0 +1,6 @@
+<?php
+
+require_once dirname(__DIR__) . '/estudiantes_app/db/conexion_db.php';
+require_once dirname(__DIR__) . 'estudiantes_app/controllers/i_controller.php';
+require_once dirname(__DIR__) . '/estudiantes_app/models/estudiantes.php';
+require_once dirname(__DIR__) . 'estudiantes_app/controllers/estudiante_controller.php';
