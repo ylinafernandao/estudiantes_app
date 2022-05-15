14490eb22dfa1d9188efd5ffc6556d9285e78843
ylinafernandao <lfotalora@uniboyaca.edu.co>
2022-05-14 23:46:27 -0500

    registrar

---
+++ registrar.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once dirname(__DIR__) . '/estudiantes_app/db/conexion_db.php';
+require_once dirname(__DIR__) . '/estudiantes_app/controllers/i_controller.php';
+require_once dirname(__DIR__) . '/estudiantes_app/models/estudiantes.php';
+require_once dirname(__DIR__) . '/estudiantes_app/controllers/estudiantes_controller.php';
+
+
